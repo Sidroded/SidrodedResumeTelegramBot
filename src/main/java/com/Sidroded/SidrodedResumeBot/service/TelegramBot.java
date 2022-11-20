2c84f1b39a87caad9611b833ff51229be3a5f550
Sidroded <deinekindaniil21@gmail.com>
2022-11-20 20:25:26 +0100

    Resume Bot version 0.9

--- src/main/java/com/Sidroded/SidrodedResumeBot/service/TelegramBot.java
+++ src/main/java/com/Sidroded/SidrodedResumeBot/service/TelegramBot.java
@@ -25,6 +25,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
     static final String IN_DEV_TEXT = "This part is still under development.";
     static final String ABOUT_DATA = "ABOUT";
+    static final String EDUCATION_DATA = "EDUCATION";
+    static final String PORTFOLIO_DATA = "PORTFOLIO";
+    static final String SOCIALS_DATA = "SOCIALS";
     static final String HELP_TEXT = "This bot was created by Daniil Deinekin (Sidroded). \n" +
             "You can use menu to get different information about me. \n\n" +
             "This bot is under development, so some features may not be available. \n\n" +
@@ -34,6 +37,14 @@ public class TelegramBot extends TelegramLongPollingBot {
             "Type /portfolio to see my projects.\n" +
             "Type /socials to see all my social media.\n" +
             "Type /help to see this massage again.\n";
+    static final String ABOUT_TEXT = "I'm Trainee / Junior Java developer. After working in sales for a long time, " +
+            "I got to know databases and information technology more, and they steal a way my to heart. " +
+            "I started taking courses and reading books like \"Java the complete reference\", \"Introducing Maven\", \"Head First Java\". " +
+            "All I have is Java basics and a wide smile, but I want to learn and achieve more. Eager to build a career in Java development." +
+            " Ready to learn and work 24/7 to get better!";
+    static final String EDUCATION_TEXT = "I studied on my own in online courses. Here is an example of sites where I received my education.";
+    static final String PORTFOLIO_TEXT = "";
+    static final String SOCIALS_TEXT = "Glad to see you here. Here you can find any of my social networks. I look forward to your connection.";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
@@ -94,16 +105,31 @@ public class TelegramBot extends TelegramLongPollingBot {
             long massageId = update.getCallbackQuery().getMessage().getMessageId();
             long chatId = update.getCallbackQuery().getMessage().getChatId();
 
-            if (callbackData.equals(ABOUT_DATA)) {
-                aboutCommandReceived(chatId);
+            switch (callbackData) {
+                case ABOUT_DATA:
+                    aboutCommandReceived(chatId);
+                    break;
+                case EDUCATION_DATA:
+                    educationCommandReceived(chatId);
+                    break;
+                case PORTFOLIO_DATA:
+                    portfolioCommandReceived(chatId);
+                    break;
+                case SOCIALS_DATA:
+                    socialsCommandReceived(chatId);
+                    break;
             }
+            /*if (callbackData.equals(ABOUT_DATA)) {
+                aboutCommandReceived(chatId);
+            }*/
         }
     }
 
     private void startCommandReceived(long chatId, String name) {
+
         SendMessage aboutMassage = new SendMessage();
         aboutMassage.setChatId(String.valueOf(chatId));
-        aboutMassage.setText("As a first step, let me introduce myself. Please press /about to show main information about me.");
+        aboutMassage.setText("As a first step, let me introduce myself. Please click /about or press the button to show main information about me.");
 
         InlineKeyboardMarkup markupinLine = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
@@ -134,24 +160,102 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error("Error occurred" + e.getMessage());
         }
-        //sendMassage(chatId, "As a first step, let me introduce myself. Please press /about to show main information about me.");
     }
 
     private void aboutCommandReceived(long chatId) {
-        sendMassage(chatId, IN_DEV_TEXT);
+
+        String educationButtonText = "Go to my education click /education or press the button.";
+
+        try {
+            sendMassage(chatId, ABOUT_TEXT);
+            Thread.sleep(1000);
+            sendMassageButton(chatId, EDUCATION_DATA, "My Education", educationButtonText, ":trophy:");
+        } catch (Exception ignored) {}
+
     }
 
     private void educationCommandReceived(long chatId) {
-        sendMassage(chatId, IN_DEV_TEXT);
+
+        String educationSite1 = "My first training site is called CodeGym. On it I learned the basics of JavaCore. " +
+                "Solved more than 500 tasks for real practical experience, and I'm not going to stop.\n\n" +
+                "https://codegym.cc";
+        String educationSite2 = "Now I am studying at EPAM online university. I like learning and improving.\n\n" +
+                "https://epam.com";
+        String educationSite3 = "I am also constantly improving on the CodeWars website. If you want you can challenge me!\n\n" +
+                "https://www.codewars.com/users/Sidroded";
+        String portfolioButtonText = "Go to my portfolio click /portfolio or press the button.";
+
+        try {
+            sendMassage(chatId, EDUCATION_TEXT);
+            Thread.sleep(2500);
+            sendMassage(chatId, educationSite1);
+            Thread.sleep(2000);
+            sendMassage(chatId, educationSite2);
+            Thread.sleep(2000);
+            sendMassage(chatId, educationSite3);
+            Thread.sleep(1000);
+            sendMassageButton(chatId, PORTFOLIO_DATA, "My Portfolio", portfolioButtonText, ":rocket:");
+        } catch (Exception ignored) {}
     }
 
     private void portfolioCommandReceived(long chatId) {
-        sendMassage(chatId, IN_DEV_TEXT);
+
+        String socialButtonText = "Go to my social media click /socials or press the button.";
+        try {
+            sendMassage(chatId, IN_DEV_TEXT);
+            Thread.sleep(1000);
+            sendMassageButton(chatId, SOCIALS_DATA, "My Social Media", socialButtonText, ":iphone:");
+        } catch (Exception ignored) {}
+
     }
 
     private void socialsCommandReceived(long chatId) {
-        sendMassage(chatId, IN_DEV_TEXT);
+
+        String mySocialMedia = "My GitHub - https://github.com/Sidroded\n" +
+                "My LinkedIn - https://www.linkedin.com/in/daniil-deinekin-13a980231/\n" +
+                "My Twitter - https://twitter.com/sidroded\n" +
+                "My Telegram - https://t.me/sidroded";
+        try {
+            sendMassage(chatId, SOCIALS_TEXT);
+            sendMassage(chatId, mySocialMedia);
+
+            Thread.sleep(1000);
+            sendMassage(chatId, EmojiParser.parseToUnicode("You are awesome, you know? " + ":smiling_face_with_hearts:"));
+        } catch (Exception ignored) {}
     }
+
+    private void sendMassageButton(long chatId, String callBackName, String nameButton, String textButtonMassage, String emoji) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textButtonMassage);
+
+        InlineKeyboardMarkup markupinLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+        var button = new InlineKeyboardButton();
+
+        button.setText(EmojiParser.parseToUnicode(nameButton + " " + emoji));
+        button.setCallbackData(callBackName);
+
+        rowInLine.add(button);
+        rowsInLine.add(rowInLine);
+        markupinLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupinLine);
+        try {
+            execute(message);
+        } catch (Exception e) {}
+    }
+
+    /*private void sendPhoto(long chatId, File photo) {
+        var message = new SendPhoto()
+                .setChatId(chatId)
+                .setCaption("Caption")
+                .setPhoto(photo);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {}
+
+    }*/
 
     private void sendMassage(long chatId, String massageSend) {
         SendMessage sendMessage = new SendMessage();
